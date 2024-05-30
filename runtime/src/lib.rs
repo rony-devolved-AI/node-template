b9201784199014f3359c973a679274c62e3e5cb1
rony-devolved-AI <rony@devolvedai.com>
2024-05-30 17:27:25 +0600

    Added pallet staking

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -292,6 +292,9 @@ mod runtime {
 	// Include the custom logic from the pallet-template in the runtime.
 	#[runtime::pallet_index(7)]
 	pub type TemplateModule = pallet_template;
+	
+	#[runtime::pallet_index(8)]
+	pub type Staking=pallet_staking;
 }
 
 /// The address format for describing accounts.
@@ -469,6 +472,69 @@ impl_runtime_apis! {
 		}
 	}
 
+	pallet_staking_reward_curve::build! {
+		const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
+			min_inflation: 0_025_000,
+			max_inflation: 0_100_000,
+			ideal_stake: 0_500_000,
+			falloff: 0_050_000,
+			max_piece_count: 40,
+			test_precision: 0_005_000,
+		);
+	}
+	
+	parameter_types! {
+		pub const SessionsPerEra: sp_staking::SessionIndex = 6;
+		pub const BondingDuration: sp_staking::EraIndex = 24 * 28;
+		pub const SlashDeferDuration: sp_staking::EraIndex = 24 * 7; // 1/4 the bonding duration.
+		pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
+		pub const MaxNominatorRewardedPerValidator: u32 = 256;
+		pub const OffendingValidatorsThreshold: Perbill = Perbill::from_percent(17);
+		pub OffchainRepeat: BlockNumber = 5;
+		pub HistoryDepth: u32 = 84;
+	}
+	
+	pub struct StakingBenchmarkingConfig;
+	impl pallet_staking::BenchmarkingConfig for StakingBenchmarkingConfig {
+		type MaxNominators = ConstU32<1000>;
+		type MaxValidators = ConstU32<1000>;
+	}
+	
+	impl pallet_staking::Config for Runtime {
+		type MaxNominations = MaxNominations;
+		type Currency = Balances;
+		type CurrencyBalance = Balance;
+		type UnixTime = Timestamp;
+		type CurrencyToVote = sp_staking::currency_to_vote::U128CurrencyToVote;
+		type RewardRemainder = Treasury;
+		type RuntimeEvent = RuntimeEvent;
+		type Slash = Treasury; // send the slashed funds to the treasury.
+		type Reward = (); // rewards are minted from the void
+		type SessionsPerEra = SessionsPerEra;
+		type BondingDuration = BondingDuration;
+		type SlashDeferDuration = SlashDeferDuration;
+		/// A super-majority of the council can cancel the slash.
+		type AdminOrigin = EitherOfDiverse<
+			EnsureRoot<AccountId>,
+			pallet_collective::EnsureProportionAtLeast<AccountId, CouncilCollective, 3, 4>,
+		>;
+		type SessionInterface = Self;
+		type EraPayout = pallet_staking::ConvertCurve<RewardCurve>;
+		type NextNewSession = Session;
+		type MaxNominatorRewardedPerValidator = MaxNominatorRewardedPerValidator;
+		type OffendingValidatorsThreshold = OffendingValidatorsThreshold;
+		type ElectionProvider = ElectionProviderMultiPhase;
+		type GenesisElectionProvider = onchain::OnChainExecution<OnChainSeqPhragmen>;
+		type VoterList = VoterList;
+		// This a placeholder, to be introduced in the next PR as an instance of bags-list
+		type TargetList = pallet_staking::UseValidatorsMap<Self>;
+		type MaxUnlockingChunks = ConstU32<32>;
+		type HistoryDepth = HistoryDepth;
+		type EventListeners = NominationPools;
+		type WeightInfo = pallet_staking::weights::SubstrateWeight<Runtime>;
+		type BenchmarkingConfig = StakingBenchmarkingConfig;
+	}
+	
 	impl pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance> for Runtime {
 		fn query_info(
 			uxt: <Block as BlockT>::Extrinsic,
